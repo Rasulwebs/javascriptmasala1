41d81736b2ccec591b6582524ad80f8f12e4e42b
Rasulwebs <boboqulovrasul80@gamil.com>
2022-08-13 00:17:35 +0500

    JavaScript_masalalar

---
+++ jshome1/homeless1.js
@@ -0,0 +1,154 @@
+"user strict";
+// Uyga ishi Masalalar
+
+
+
+// ===== 2.masala =====
+
+// let xson = +prompt('sonno kiriting')
+// if (xson % 2 == 0) {
+//     console.log('Ok! Juft son')
+// } else {
+//     console.log('No! Toq son')
+// }
+
+
+//  =========  4. masala ========
+
+// let a = +prompt("ason kiriting");
+// let b = (Math.floor(a/ 10) * 10)
+// let c = a %= 10
+// console.log(`${b}o'nlik ${c}birlik`)
+
+// ======= 15.masala  ========
+
+// let a = +prompt("1-son kiriting");
+// let b = +prompt("2-son kiriting");
+// let c = +prompt("3-son kiriting");
+// let d = +prompt("4-son kiriting");
+
+// if (a > 0 && b > 0 && c > 0 && d > 0) {
+//     console.log(true);
+// }
+
+
+// ========  6.masala  =========
+
+
+// let a = +prompt("sonni kiriting");
+// let first = Math.floor(a/ 10);
+// let second = a % 10;
+
+// alert(first * second)
+
+
+//  ======== 7.masala  =========
+
+// let a = +prompt("sonni kiriting");
+
+// let b = (Math.floor(a / 100) * 100);
+// let c = a %= 100;
+// console.log(b);
+
+// console.log(c);
+
+
+// ========= 5.masala =========  
+
+// let xson = +prompt("sonni kiriting:");
+
+// let firstn = (Math.floor(xson / 10));
+// let secondn = xson % 10;
+
+
+// console.log(firstn + secondn);
+
+
+
+// ======== 8.masala =========
+
+// let xson = +prompt("sonni tanlang");
+
+// let hour = 10000;
+// let minut = 60;
+
+// let hournumb = Math.floor(xson/hour);
+// let minutnumb = xson %= hour;
+// let sekundnumb = minutnumb %= minut;
+// alert(`${hournumb}soat, ${minutnumb}minut, ${sekundnumb}sekund`);
+
+
+// ======== 14.masala  =========
+
+// let a = +prompt ("a son kiriting");
+
+// let b = +prompt("b son kiriting");
+// let c = +prompt("c son kiriting");
+
+// if (a < b && b < c) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+
+// ========  13.masala  =========
+
+// let x = +prompt("sonni kiriting");
+
+// if (x < 0) {
+//     console.log("manfiy son");
+// } else {
+//     console.log("musbat son");
+// }
+
+
+// ========== 18.masala ===========
+
+// let a = +prompt("Kod orqali kompaniyani aniqlaymiz");
+// if (a == 99 || a == 95) {
+//     console.log("Uzmobile")
+// } 
+// else if (a == 94 || a == 93) {
+//     console.log("Ucell");
+
+// } 
+// else if (a == 90) {
+//     console.log("Beline")
+// } 
+// else if (a == 97 || a == 78) {
+//     console.log("UMS")
+// } 
+// else {
+//     let a = +prompt("Xato!");
+    
+//     if (a == 99 || a == 95) {
+//         console.log("Uzmobile")
+//     } 
+//     else if (a == 94 || a == 93) {
+//         console.log("Ucell");
+    
+//     } 
+//     else if (a == 90) {
+//         console.log("Beeline")
+//     } 
+//     else if (a == 97 || a == 78) {
+//         console.log("UMS")
+//     } 
+   
+//     };
+
+
+
+
+
+
+
+
+
+
+//  ======  1.masala  ========
+
+
+// let a = +prompt("sonni metrga aylantramiz");
+// console.log(a / 100+"m");
